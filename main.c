a11ad0376451d4d39cd825c399032b2e74ea792e
noobella384 <162159734+noobella384@users.noreply.github.com>
2024-12-30 04:52:10 +0530

    Slave files

--- main.c
+++ main.c
@@ -1,40 +1,52 @@
 /*
- * SPI1.c
+ * SPI2.c
  *
- * Created: 25-03-2024 18:14:01
+ * Created: 25-03-2024 22:48:36
  * Author : Dev Arora
  */ 
-
 #define F_CPU 16000000UL
 #include <avr/io.h>
 #include <util/delay.h>
 #include <stdlib.h>
 #include <stdio.h>
 #define Bud 9600UL
+#define ubrr ((F_CPU / (Bud*16)) - 1)
 
-int main(void){
-	DDRA=0xff;
+void UART_init(unsigned long BUD)
+{
+	UCSR0B = UCSR0B | (1<< TXEN0);
+	UCSR0C =  (1<< UCSZ00) | (1<<UCSZ01);
+	UBRR0L= ubrr;
+	UBRR0H= ubrr >> 8;
+	
+}
+void UART_TxChar(char ch){
+	while (! (UCSR0A & (1<<UDRE0)));
+	UDR0=ch;
+}
+
+char SPI_SlaveReceive(void)
+{
+	while(!(SPSR & (1<<SPIF)));
+	PORTA^=0xff;
+	return SPDR;
+}
+
+int main(void)
+{
+	
+	DDRB =DDRB | (1<<3);
+	SPCR=SPCR | (1 <<SPE);
+	UART_init(9600);
 	PORTA=0xff;
 	_delay_ms(1000);
-	DDRB=DDRB | (1<<2) |(1<<0);
-	PORTB=PORTB & 0b11111110;
-	DDRB=DDRB | (1<<1);
-	SPCR= SPCR | (1<<SPE) | (1<<MSTR) | (1<<SPR0);
-	
 	while(1){
-	SPDR='d';
-    while (!(SPSR & (1<<SPIF)));
-	_delay_ms(100);
-	SPDR='e';
-	while (!(SPSR & (1<<SPIF)));
-	_delay_ms(100);
-	SPDR='v';
-	while (!(SPSR & (1<<SPIF)));
-	_delay_ms(100);
-	SPDR=' ';
-	while (!(SPSR & (1<<SPIF)));
-	_delay_ms(100);
-	}
-	return 0;
+	char c=SPI_SlaveReceive();
+	_delay_ms(50);
+	UART_TxChar(c);
+	_delay_ms(50);
 }
+return 0;
+}
+ 
 
