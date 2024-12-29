99112f503c2b176c85f49c4391dfe127cf6b3ef7
noobella384 <162159734+noobella384@users.noreply.github.com>
2024-12-30 05:01:08 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,18 @@
+/*
+ * GccApplication5.c
+ *
+ * Created: 07-07-2024 00:05:31
+ * Author : Dev Arora
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
